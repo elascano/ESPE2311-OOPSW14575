bb28491c90fce79acc78b7f3ea440b9bc3c1ce8e
CarlosHa44 <carlosha176@gmail.com>
2023-11-21 23:39:56 -0500

    update hw

---
+++ hw/hernandezc/u1/hw05chickenfarm/ChickenFarm/src/ec/edu/espe/chickenfarm/model/Chicken.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.chickenfarm.model;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Chicken {
+    private int id;
+    private String name;
+    private String color;
+    private int age;
+    private boolean isMolting;
+    
+    public void doStuff(){
+    }
+    private void cluck(){
+    }
+    private void wander(){
+    }
+    private void eat(){
+    }
+    private void drink(){
+    }
+    private Poop poop(){
+        return null;
+    }
+    private Egg layAnEgg(){
+        return null;
+    }
+}
