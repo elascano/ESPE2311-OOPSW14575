46c501f15c8fba596db6c510dc0fb59384a19009
RevillaA <revillaantonio212005@hotmail.com>
2023-11-21 21:35:40 -0500

    adding hw01-06

---
+++ hw/revillaa/unit 1/HW03ObjectsAndClassesDependencies/Chicken.java
@@ -0,0 +1,22 @@
+public class Chicken{
+	int id;
+	String name;
+	String color;
+	int age;
+	boolean IsMolting;
+	
+	public void doStuff(int forTime){
+	}
+	private void cluck(){
+	}
+	private void wander(){
+	}
+	private void eat(){
+	}
+	private void drink(){
+	}
+	private void Poop poop(){
+	}
+	private void Egg layAnEgg(){
+	}
+}
---
+++ hw/revillaa/unit 1/HW03ObjectsAndClassesDependencies/ChickenCoop.java
@@ -0,0 +1,9 @@
+public class ChickenCoop {
+	int id;
+	Chicken[] chickens;
+
+	public void add(Chicken chicken);
+	public void remove(int chicken);
+	public void restlteration();
+	public void Chicken next();
+}
