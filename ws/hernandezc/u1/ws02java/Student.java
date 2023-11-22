a61b8697718ad520efa02db3f2b01fa7f9b16433
CarlosHa44 <carlosha176@gmail.com>
2023-11-22 00:00:51 -0500

    update ws

---
+++ ws/hernandezc/u1/ws02java/Student.java
@@ -0,0 +1,15 @@
+public class Student{
+	private int id;
+	private String name;
+	private int bornOnYear;
+	private int bornOnMonth;
+	private int bornOnDay;
+	private int ageInYears;
+	private String gender;
+	private String major;
+	private float finalGrade;
+	private String currentSemester;
+	private String fatherName;
+}
+
+
