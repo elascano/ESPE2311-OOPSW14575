eddaec3ef17ff3d68a0f1fd6af2ccdd0ce4d97da
SEBASTIAN_CRIOLLO <sebastiancriollo943@gmail.com>
2023-11-29 21:51:25 -0500

    update wh and workshops

--- ws/criollob/UNIT 1/WS07ClassConstructors/ChickenFarm/ChickenFarm/src/ec/edu/espe/chickenfarm/model/Chicken.java
+++ ws/criollob/UNIT 1/WS07ClassConstructors/ChickenFarm/ChickenFarm/src/ec/edu/espe/chickenfarm/model/Chicken.java
@@ -14,7 +14,7 @@ public class Chicken {
 
     @Override
     public String toString() {
-        return "Chicken{" + "id=" + id + ", name=" + name + ", color=" + color + ", age=" + age + ", isMolting=" + isMolting + '}';
+        return "Chicken{" + "id=" + getId() + ", name=" + getName() + ", color=" + getColor() + ", age=" + getAge() + ", isMolting=" + isIsMolting() + '}';
     }
 
     public Chicken(int id, String name, String color, int age, boolean isMolting) {
@@ -25,6 +25,75 @@ public class Chicken {
         this.isMolting = isMolting;
     }
 
-   
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the isMolting
+     */
+    public boolean isIsMolting() {
+        return isMolting;
+    }
+
+    /**
+     * @param isMolting the isMolting to set
+     */
+    public void setIsMolting(boolean isMolting) {
+        this.isMolting = isMolting;
+    }
+
     
 }
