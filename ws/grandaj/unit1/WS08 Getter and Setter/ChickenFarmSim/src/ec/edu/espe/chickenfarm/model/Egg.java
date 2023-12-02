a3ead7cee39d37f96cb67412fb48e1d666da9f74
Juan <juan.granda342464gmail.com>
2023-12-01 22:25:05 -0500

    HW AND WS UPDATE

---
+++ ws/grandaj/unit1/WS08 Getter and Setter/ChickenFarmSim/src/ec/edu/espe/chickenfarm/model/Egg.java
@@ -0,0 +1,24 @@
+
+package ec.edu.espe.chickenfarm.model;
+
+/**
+ *
+ * @author JuanGranda,Error 404,DCCO-ESPE
+ */
+public class Egg {
+    private int id;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+}
