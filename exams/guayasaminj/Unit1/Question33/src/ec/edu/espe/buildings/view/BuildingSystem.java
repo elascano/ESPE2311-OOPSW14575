892da42dcd0b819c55a5fbe788ec7168ab36cf4d
GjosueD <Josue>
2023-12-21 00:31:34 -0500

    Examen1

---
+++ exams/guayasaminj/Unit1/Question33/src/ec/edu/espe/buildings/view/BuildingSystem.java
@@ -0,0 +1,20 @@
+
+package ec.edu.espe.buildings.view;
+
+import java.util.Scanner;
+import ec.edu.espe.buildings.model.Building;
+
+
+/**
+ *
+ * @author JosuéG
+ */
+public class BuildingSystem {
+   static Scanner scan = new Scanner(System.in);
+     public static void main(String[] args) {
+        
+         
+         
+        
+    }
+}
