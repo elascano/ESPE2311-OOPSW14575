29cadbb81eac830351b6e3e2907394876d008108
Cotbert <mateojgdev@gmail.com>
2024-01-08 05:16:44 -0500

    ws and hw updates

---
+++ ws/garciam/u2/ws03_oop_in_python/findEcuation/main.py
@@ -0,0 +1,8 @@
+import EcuationFinder
+
+def main():
+    ecuation_finder = EcuationFinder.EquationFinder("./tramo3.csv")
+    ecuation_finder.findEcuation()
+
+if __name__ == "__main__":
+    main()
