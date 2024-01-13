71226f28dac4e7026e49be3aedfc1052e6692ed6
aarzec <andresrz2k21@gmail.com>
2024-01-13 08:00:56 -0500

    Added WS26 Code

---
+++ ws/romeroa/unit2/WS26_Polymorphism/Code/src/main/java/ec/edu/espe/zoo/model/Animal.java
@@ -0,0 +1,55 @@
+package ec.edu.espe.zoo.model;
+
+import java.util.Date;
+
+public class Animal {
+    private int id;
+    private String scientificName;
+    private Date bornDate;
+    private int cageId;
+
+    public Animal(int id, String scientificName, Date bornDate, int cageId) {
+        this.id = id;
+        this.scientificName = scientificName;
+        this.bornDate = bornDate;
+        this.cageId = cageId;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal [id=" + id + ", scientificName=" + scientificName + ", bornDate=" + bornDate + ", cageId="
+                + cageId + "]";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public Date getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(Date bornDate) {
+        this.bornDate = bornDate;
+    }
+
+    public int getCageId() {
+        return cageId;
+    }
+
+    public void setCageId(int cageId) {
+        this.cageId = cageId;
+    }
+}
