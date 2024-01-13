689d6304402b2e565dec6df8447439b97acfd39f
aarzec <andresrz2k21@gmail.com>
2024-01-13 06:41:57 -0500

    Corrected WS25

--- ws/romeroa/unit2/WS25_ZooExample/Code/src/main/java/ec/edu/espe/zoo/view/Zoo.java
+++ ws/romeroa/unit2/WS25_ZooExample/Code/src/main/java/ec/edu/espe/zoo/view/Zoo.java
@@ -9,13 +9,13 @@ import ec.edu.espe.zoo.model.Platypus;
 public class Zoo {
     public static void main(String[] args) {
         Animal animal = new Animal(0, "platypus", new Date(), 1);
-        System.out.println("animal -->" + animal);
-        System.out.println("animaltType" + animal.getClass());
+        System.out.println("animal --> " + animal);
+        System.out.println("animaltType " + animal.getClass());
 
-        animal = new Platypus(true, 0, 0, "spine small", 0, 0, "platypus scientific", new Date(), 0);
-        System.out.println("Animal -->" + animal);
+        animal = new Platypus(true, 0, 0, "spine small", 0, 0, "platypus scientific ", new Date(), 0);
+        System.out.println("animal --> " + animal);
         System.out.println(animal.toString());
 
-        animal = new Giraffe(0, "xd", 0, 0, "xd", new Date(), 0);
+        animal = new Giraffe(0, "Dorsal", 0, 0, "giraffus commonalis", new Date(), 0);
     }
 }
