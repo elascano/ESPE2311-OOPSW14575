5fa81306901f1d9db82f1f8c081debf8abecf250
Teo344 <gabriel2004mat@gmail.com>
2024-01-15 21:26:53 -0500

    Upload hw16AbstractionOverrideFunctions

---
+++ hw/criollom/u2/hw16AbstractionOverrideFunctions/Zoo/src/ec/espe/edu/zoo/model/Animal.java
@@ -0,0 +1,101 @@
+
+package ec.espe.edu.zoo.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mateo Gabriel Criollo/OOP14575/InterByte
+ */
+public abstract class Animal {
+    private int id;
+    private String ciemtificName;
+    private Date bornOnDate;
+    private int cageId;
+
+    public Animal(int id, String ciemtificName, Date bornOnDate, int cageId) {
+        this.id = id;
+        this.ciemtificName = ciemtificName;
+        this.bornOnDate = bornOnDate;
+        this.cageId = cageId;
+    }
+
+    @Override
+    public String toString() {
+        return "\n-Animal{" + "id=" + id + ", ciemtificName=" + ciemtificName + ", bornOnDate=" + bornOnDate + ", cageId=" + cageId + "}\n";
+    }
+    
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the ciemtificName
+     */
+    public String getCiemtificName() {
+        return ciemtificName;
+    }
+
+    /**
+     * @param ciemtificName the ciemtificName to set
+     */
+    public void setCiemtificName(String ciemtificName) {
+        this.ciemtificName = ciemtificName;
+    }
+
+    /**
+     * @return the bornOnDate
+     */
+    public Date getBornOnDate() {
+        return bornOnDate;
+    }
+
+    /**
+     * @param bornOnDate the bornOnDate to set
+     */
+    public void setBornOnDate(Date bornOnDate) {
+        this.bornOnDate = bornOnDate;
+    }
+
+    /**
+     * @return the cageId
+     */
+    public int getCageId() {
+        return cageId;
+    }
+
+    /**
+     * @param cageId the cageId to set
+     */
+    public void setCageId(int cageId) {
+        this.cageId = cageId;
+    }
+    
+    public abstract void feed();
+        
+    public void assingCage(int id){
+        System.out.println("cage id new --->"+id);
+        cageId=id;
+    }
+    
+    public abstract void breathe();
+    
+   public void registerMetamorphose(boolean confirmation, String name){
+       if (confirmation){
+           System.out.println("The "+name+" animal is in metamorphosis");
+       }else{
+           System.out.println("The "+name+" animal is not  in metamorphosis");
+       }
+   }
+}
---
+++ hw/criollom/u2/hw16AbstractionOverrideFunctions/Zoo/src/ec/espe/edu/zoo/model/Invertebrate.java
@@ -0,0 +1,117 @@
+
+package ec.espe.edu.zoo.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mateo Gabriel Criollo/OOP14575/InterByte
+ */
+public abstract class Invertebrate extends Animal {
+    private String symmetry;
+    private int quantityOfLimbs;
+    private String feedType;
+    private String habitad;
+    private String reproduction;
+
+    public Invertebrate(String symmetry, int quantityOfLimbs, String feedType, String habitad, String reproduction, int id, String ciemtificName, Date bornOnDate, int cageId) {
+        super(id, ciemtificName, bornOnDate, cageId);
+        this.symmetry = symmetry;
+        this.quantityOfLimbs = quantityOfLimbs;
+        this.feedType = feedType;
+        this.habitad = habitad;
+        this.reproduction = reproduction;
+    }
+
+    @Override
+    public String toString() {
+        return "Invertebrate{" +super.toString()+ "symmetry=" + getSymmetry() + ", quantityOfLimbs=" + getQuantityOfLimbs() + ", feedType=" + getFeedType() + ", habitad=" + getHabitad() + ", reproduction=" + getReproduction() + '}';
+    }
+
+    /**
+     * @return the symmetry
+     */
+    public String getSymmetry() {
+        return symmetry;
+    }
+
+    /**
+     * @param symmetry the symmetry to set
+     */
+    public void setSymmetry(String symmetry) {
+        this.symmetry = symmetry;
+    }
+
+    /**
+     * @return the quantityOfLimbs
+     */
+    public int getQuantityOfLimbs() {
+        return quantityOfLimbs;
+    }
+
+    /**
+     * @param quantityOfLimbs the quantityOfLimbs to set
+     */
+    public void setQuantityOfLimbs(int quantityOfLimbs) {
+        this.quantityOfLimbs = quantityOfLimbs;
+    }
+
+    /**
+     * @return the feedType
+     */
+    public String getFeedType() {
+        return feedType;
+    }
+
+    /**
+     * @param feedType the feedType to set
+     */
+    public void setFeedType(String feedType) {
+        this.feedType = feedType;
+    }
+
+    /**
+     * @return the habitad
+     */
+    public String getHabitad() {
+        return habitad;
+    }
+
+    /**
+     * @param habitad the habitad to set
+     */
+    public void setHabitad(String habitad) {
+        this.habitad = habitad;
+    }
+
+    /**
+     * @return the reproduction
+     */
+    public String getReproduction() {
+        return reproduction;
+    }
+
+    /**
+     * @param reproduction the reproduction to set
+     */
+    public void setReproduction(String reproduction) {
+        this.reproduction = reproduction;
+    }
+    
+    public void specialMove(){
+        
+    }
+    
+    public void pair(){
+        
+    }  
+            
+    public void establishHabitat(){
+        
+    }
+
+    @Override
+    public void feed() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
