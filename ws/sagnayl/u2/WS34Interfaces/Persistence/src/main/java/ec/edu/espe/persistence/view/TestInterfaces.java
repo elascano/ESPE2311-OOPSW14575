5c8cc1df0fe09676671396ffd201fa9c8ef4463d
Luis Sagnay <sagnayluis99@gmail.com>
2024-01-28 22:15:43 -0500

    WS

--- ws/sagnayl/u2/WS34Interfaces/Persistence/src/main/java/ec/edu/espe/persistence/view/TestInterfaces.java
+++ ws/sagnayl/u2/WS34Interfaces/Persistence/src/main/java/ec/edu/espe/persistence/view/TestInterfaces.java
@@ -10,7 +10,7 @@ import ec.edu.espe.persistence.model.Country;
 public class TestInterfaces {
     public static void main(String[] args) {
         Country country = new Country("Argentina", 8900, "Buenos Aires");
-        MongoManager mongodb = new MongoManager("mongodb+srv://luis:luis22@cluster0.h5n9yna.mongodb.net/?retryWrites=true&w=majority", "DPEXSystemDB");
+        MongoManager mongodb = new MongoManager(System.getProperty("URI"), "DPEXSystemDB");
         mongodb.create(country, "Country");
 
     }
