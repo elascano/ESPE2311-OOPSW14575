85989fc16ffd4a2a5c051923691a9571c0a3f36e
Cepeda Salguero David Gustavo <150314536+davidcepeda1@users.noreply.github.com>
2024-01-29 21:52:06 -0500

    WS25ZooExample added

---
+++ ws/cepedad/Unit 2/WS25ZooExample/Zoo/src/ec/edu/espe/zoo/model/Giraffe.java
@@ -0,0 +1,35 @@
+package ec.edu.espe.zoo.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author David
+ */
+public class Giraffe extends Mammal {
+
+    public Giraffe(int numberOfMammaryGlands, String spine, int numberOfBones, int id, String scientificName, Date bornDate, int cageId) {
+        super(numberOfMammaryGlands, spine, numberOfBones, id, scientificName, bornDate, cageId);
+    }
+    
+    @Override
+    public void breastfeed(){
+    System.out.println("giraffe is breastfeeding");
+}
+   public void breastfeed(int calf){
+       System.out.println("giraffe is breastfeeding its babe number --> " + calf);
+   }
+
+    @Override
+    public String toString() {
+        return "Giraffe{" + super.toString() + '}';
+    }
+    
+    public void brushNeck(){
+        System.out.println("brushing the neck of the giraffe");
+    }
+
+    @Override
+    public void feed() {
+        System.out.println("Feeding giraffe with grass");    }
+}
