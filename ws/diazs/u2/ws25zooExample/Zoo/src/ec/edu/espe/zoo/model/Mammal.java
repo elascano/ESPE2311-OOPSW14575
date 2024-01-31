55e1b9949d9125294ef251d6bd93605516f6a8d9
Steft91 <stefany152005@gmail.com>
2024-01-30 23:02:11 -0500

    Update ws30

---
+++ ws/diazs/u2/ws25zooExample/Zoo/src/ec/edu/espe/zoo/model/Mammal.java
@@ -0,0 +1,40 @@
+package ec.edu.espe.zoo.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Stefany Díaz, HoneyBadgers, DCCO - ESPE
+ */
+public class Mammal extends Vertebrate{
+    private int numberOfMammaryGlands;
+    
+    public void breastfeed(){
+        System.out.println("mammal breastfeed baby 1");
+    }
+
+    @Override
+    public String toString() {
+        return "Mammal{"+ super.toString() + "numberOfMammaryGlands=" + numberOfMammaryGlands + '}';
+    }
+
+    public Mammal(int numberOfMammaryGlands, String spine, int numberOfBones, int id, String scientificName, Date bornDate, int cageId) {
+        super(spine, numberOfBones, id, scientificName, bornDate, cageId);
+        this.numberOfMammaryGlands = numberOfMammaryGlands;
+    }
+
+    /**
+     * @return the numberOfMammaryGlands
+     */
+    public int getNumberOfMammaryGlands() {
+        return numberOfMammaryGlands;
+    }
+
+    /**
+     * @param numberOfMammaryGlands the numberOfMammaryGlands to set
+     */
+    public void setNumberOfMammaryGlands(int numberOfMammaryGlands) {
+        this.numberOfMammaryGlands = numberOfMammaryGlands;
+    }
+    
+}
