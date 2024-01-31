10ef06aacfeba98f0e7353d17d1f9ca4e557b087
Teo344 <gabriel2004mat@gmail.com>
2024-01-31 14:27:26 -0500

    Upload ws36Widegets

---
+++ ws/criollom/u2/ws36Widgets/Contacts/src/main/java/ar/charlycimino/contacts/Contacts.java
@@ -0,0 +1,14 @@
+
+
+package ar.charlycimino.contacts;
+
+/**
+ *
+ * @author Mateo Gabriel Criollo/OOP14575/InterByte
+ */
+public class Contacts {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
