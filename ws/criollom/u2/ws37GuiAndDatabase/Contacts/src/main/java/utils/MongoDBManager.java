c0ddfb3738f634fc232bce143dfd60a69b64ffeb
Teo344 <gabriel2004mat@gmail.com>
2024-02-01 21:02:46 -0500

    Upload ws37GuiAndDataBase and modifie hw36Widgets

---
+++ ws/criollom/u2/ws37GuiAndDatabase/Contacts/src/main/java/utils/MongoDBManager.java
@@ -0,0 +1,36 @@
+
+package utils;
+
+/**
+ *
+ * @author Mateo Gabriel Criollo/OOP14575/InterByte
+ */
+public class MongoDBManager {
+    
+    
+    
+
+    public static void create(String data, String collection){
+        //Todo create Collection documents
+    }
+    
+    public static String read(String collection){
+        //Todo return all the collection documents
+        return "data form data base";
+    }
+    
+    public static String find(String search , String collection){
+        //Todo retunr
+        return "data form data base";
+    }
+    
+    public static boolean update(String newData , String search, String collection){
+        //Todo ipdate
+        return true;
+    }
+    
+    public static boolean delete(String search , String collection){
+        //Todo create Collection
+        return true;
+    }
+}
