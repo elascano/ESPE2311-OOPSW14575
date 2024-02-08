9d9030ac38f04b486fabad66ab222da1da906cef
unknown <alexdcy27@gmail.com>
2024-02-08 03:24:00 -0500

    Exam U2 digram and captures

---
+++ exams/Cuzcoa/Unit 2/CodeOfDiagram/CodeofDiagram/src/main/java/ec/edu/espe/view/View.java
@@ -0,0 +1,12 @@
+
+package ec.edu.espe.view;
+
+/**
+ *
+ * @author Alex Cuzco, HoneyBadgers, DCCO-ESPE
+ */
+public class View {
+     public void displayInformation(String information) {
+        System.out.println(information);
+    }
+}
