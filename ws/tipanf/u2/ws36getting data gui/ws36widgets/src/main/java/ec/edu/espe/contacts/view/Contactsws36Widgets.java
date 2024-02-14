1f80c87e1606b29cbca3558809bf5390a96a88a8
Frederick-Tipan <fredericktimo@gmial.com>
2024-02-13 22:16:15 -0500

    uploaded hw and ws u2

---
+++ ws/tipanf/u2/ws36getting data gui/ws36widgets/src/main/java/ec/edu/espe/contacts/view/Contactsws36Widgets.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.contacts.view;
+
+/**
+ *
+ * @author Frederick Tipán
+ */
+public class Contactsws36Widgets {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
