bccdedfd7426397a74f331b0aa853d06245010ec
unknown <materoge32@gmail.com>
2024-02-28 23:01:31 -0500

    Unit03

---
+++ hw/rogeronm/Unit03/MazeGenerator/src/main/java/ec/edu/espe/Maze/model/CellE.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.Maze.model;
+
+import ec.edu.espe.Maze.model.door;
+
+/**
+ *
+ * @author EDWARICHSNAKE
+ */
+public class CellE extends Cell{
+    
+    public CellE(int[][] cell) {
+        super(cell);
+        System.out.println("print");
+    }
+    
+    
+    
+}
