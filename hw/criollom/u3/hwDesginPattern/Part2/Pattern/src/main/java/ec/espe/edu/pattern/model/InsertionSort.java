f7652598465900204e953f8a07e0d23afcfc50d3
Teo344 <gabriel2004mat@gmail.com>
2024-03-10 08:57:32 -0500

    Upload hw Dessign Pattern

---
+++ hw/criollom/u3/hwDesginPattern/Part2/Pattern/src/main/java/ec/espe/edu/pattern/model/InsertionSort.java
@@ -0,0 +1,15 @@
+
+package ec.espe.edu.pattern.model;
+
+/**
+ *
+ * @author Mateo Gabriel Criollo/OOP14575/InterByte
+ */
+public class InsertionSort implements SortingStrategy {
+
+    @Override
+    public int[] sort(int[] data) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
